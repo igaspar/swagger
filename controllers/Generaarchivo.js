c9cde979f77eefed349f03f9470d43ffb53398d3
aaronanzen <adelgado@anzen.com.mx>
2017-04-07 14:06:50 -0500

    Delete Generaarchivo.js

--- controllers/Generaarchivo.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var url = require('url');
-
-var Generaarchivo = require('./GeneraarchivoService');
-
-module.exports.consulta = function consulta (req, res, next) {
-  Generaarchivo.consulta(req.swagger.params, res, next);
-};
