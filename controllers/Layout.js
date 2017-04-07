b238a8c50dce311c5cd90cff1c73427567d44ff1
aaronanzen <adelgado@anzen.com.mx>
2017-04-07 13:55:33 -0500

    Add files via upload

---
+++ controllers/Layout.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var url = require('url');
+
+var Layout = require('./LayoutService');
+
+module.exports.file Definition POST = function file Definition POST (req, res, next) {
+  Layout.file Definition POST(req.swagger.params, res, next);
+};
