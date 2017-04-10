fb6c0f8a6fae300922c89733e8315aadb1dd7759
aaronanzen <adelgado@anzen.com.mx>
2017-04-10 14:08:25 -0500

    se  eliminan archivos

--- controllers/GeneraArchivo.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var url = require('url');
-
-var GeneraArchivo = require('./GeneraArchivoService');
-
-module.exports.consulta = function consulta (req, res, next) {
-  GeneraArchivo.consulta(req.swagger.params, res, next);
-};
--- controllers/Layout.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var url = require('url');
-
-var Layout = require('./LayoutService');
-
-module.exports.consultas = function consultas (req, res, next) {
-  Layout.consultas(req.swagger.params, res, next);
-};
