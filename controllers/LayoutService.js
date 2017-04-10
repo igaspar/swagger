cb8737e91868d540bde03394b5b22d449fea2dbb
aaronanzen <adelgado@anzen.com.mx>
2017-04-10 18:38:42 -0500

    se agregan archivos

--- controllers/LayoutService.js
+++ controllers/LayoutService.js
@@ -341,7 +341,7 @@ exports.layoutPOST = function(args, res, next) {
       }
    }
 }
-];
+ ];
   if (Object.keys(examples).length > 0) {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
