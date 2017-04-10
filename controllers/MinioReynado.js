7d9e56625ac24d3ec85d6bce88c9baab4155a1d3
aaronanzen <adelgado@anzen.com.mx>
2017-04-10 13:52:33 -0500

    se agrega la parte de layaout

--- controllers/MinioReynado.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var url = require('url');
-
-var MinioReynado = require('./MinioReynadoService');
-
-module.exports.minioGET = function minioGET (req, res, next) {
-  MinioReynado.minioGET(req.swagger.params, res, next);
-};
